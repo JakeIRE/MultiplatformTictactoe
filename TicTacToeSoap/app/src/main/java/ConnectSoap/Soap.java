8f3a679aacf226c53bc58510f91ba7dc96d1dcf1
Jake <15160386@studentmail.ul.ie>
2017-11-14 12:44:40 +0000

    Working? ksoap?

--- TicTacToeSoap/app/src/main/java/ConnectSoap/Soap.java
+++ TicTacToeSoap/app/src/main/java/ConnectSoap/Soap.java
@@ -37,6 +37,23 @@ public class Soap{
                 return "Error";
             }
 
+            public void setOffline(String uname){
+
+                SoapObject request = new SoapObject(NAMESPACE, "setOffline");
+                request.addProperty("u", uname);
+
+                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+
+                HttpTransportSE ht = new HttpTransportSE(URL);
+                try {
+                    ht.call(SOAP_ACTION, envelope);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
 
                    /* fromProp.setName("FromCurrency");
                     fromProp.setValue(fromCurrency);
