c97fcb13cba2664689e61c3126b019c3333c106e
Colum <15142434@studentmail.ul.ie>
2017-11-15 19:37:40 +0000

    ads

--- TicTacToeSoap/app/src/main/java/colummullallycom/tictactoesoap/LeaderBoard.java
+++ TicTacToeSoap/app/src/main/java/colummullallycom/tictactoesoap/LeaderBoard.java
@@ -19,12 +19,9 @@ public class LeaderBoard extends AppCompatActivity {
         Bundle b = iin.getExtras();
         uName = (String) b.get("Code");
         leaderboard=(ListView)findViewById(R.id.Leaderboard);
-<<<<<<< HEAD
         Soap db= new Soap();
         String[] values=db.getLeaderboard();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
         leaderboard.setAdapter(adapter);
-=======
->>>>>>> parent of 83f421c... it cant hurt too much -jake craven 17:57 15/11/2017
     }
 }
