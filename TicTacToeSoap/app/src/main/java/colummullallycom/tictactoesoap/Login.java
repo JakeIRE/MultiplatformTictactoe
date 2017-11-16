9aac26e4882b02297ba6ee4a61f212a1e20bb3bf
Jake <15160386@studentmail.ul.ie>
2017-11-16 14:55:12 +0000

    hdfh

--- TicTacToeSoap/app/src/main/java/colummullallycom/tictactoesoap/Login.java
+++ TicTacToeSoap/app/src/main/java/colummullallycom/tictactoesoap/Login.java
@@ -1,6 +1,7 @@
 package colummullallycom.tictactoesoap;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,13 +17,14 @@ public class Login extends AppCompatActivity {
     String uName,password;
     EditText passwordIn;
     Button sBtn,rBtn;
-    TextView t;
+    TextView t, updates;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
         uNameIn = (EditText) findViewById(R.id.editText);
         uNameIn.setText("");
+        updates = (TextView) findViewById(R.id.InfoField);
         passwordIn = (EditText) findViewById(R.id.editText2);
         sBtn = (Button) findViewById(R.id.button2);
         rBtn = (Button) findViewById(R.id.button);
@@ -39,16 +41,24 @@ public class Login extends AppCompatActivity {
                 password=passwordIn.getText().toString();
                 //TODO
                 //checks and stuff
-                Soap db =new Soap();
-                String output = null;
-                output = db.loginVerify(uName, password);
-                t.setText(output);
-               if(output.equals("")){
-                Intent myIntent=new Intent(Login.this,Menu.class);
-                Bundle bundle= new Bundle();
-                bundle.putString("Code",uName);
-                myIntent.putExtras(bundle);
-                startActivity(myIntent);}
+                if(uName.length() < 1 || password.length() < 1) {
+                    Soap db = new Soap();
+                    String output = null;
+                    output = db.loginVerify(uName, password);
+                    updates.setText(output);
+                    updates.setBackgroundColor(Color.RED);
+                    if (output.equals("")) {
+                        Intent myIntent = new Intent(Login.this, Menu.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("Code", uName);
+                        myIntent.putExtras(bundle);
+                        startActivity(myIntent);
+                    }
+                }
+                   else{
+                       updates.setText("Please fill the data fields");
+                       updates.setBackgroundColor(Color.RED);
+                   }
                 
 
 
@@ -58,21 +68,36 @@ public class Login extends AppCompatActivity {
         rBtn.setOnClickListener(new OnClickListener(){
             @Override
             public void onClick(View v){
-                uName = uNameIn.getText().toString();
-                password=passwordIn.getText().toString();
-                //TODO
-                //checks and stuff
-                Soap db =new Soap();
-                String output = null;
-                output = db.register(uName, password);
-                t.setText(output);
-                if(output.equals("")){
-                    Intent myIntent=new Intent(Login.this,Menu.class);
-                    Bundle bundle= new Bundle();
-                    bundle.putString("Code",uName);
-                    myIntent.putExtras(bundle);
-                    startActivity(myIntent);}
-            }
+                    uName = uNameIn.getText().toString();
+                    password=passwordIn.getText().toString();
+                    //TODO
+                    //checks and stuff
+                    if(uName.length() < 1 || password.length() < 1) {
+                        if(uName.length() < 21) {
+                            Soap db = new Soap();
+                            String output = null;
+                            output = db.loginVerify(uName, password);
+                            updates.setText(output);
+                            updates.setBackgroundColor(Color.RED);
+                            if (output.equals("")) {
+                                Intent myIntent = new Intent(Login.this, Menu.class);
+                                Bundle bundle = new Bundle();
+                                bundle.putString("Code", uName);
+                                myIntent.putExtras(bundle);
+                                startActivity(myIntent);
+                            }
+                        }
+                        else{
+                            updates.setText("Username too long");
+                            updates.setBackgroundColor(Color.RED);
+                        }
+                    }
+                        else{
+                            updates.setText("Please fill the data fields");
+                            updates.setBackgroundColor(Color.RED);
+                        }
+
+                    }
         });
     }
 
